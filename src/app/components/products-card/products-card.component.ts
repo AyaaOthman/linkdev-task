0cee61187ec6fb6f0e77a778a205e1867ab6dafd
Aya Othman <69984725+AyaaOthman@users.noreply.github.com>
2024-02-21 11:28:47 +0200

    things we do section

--- src/app/components/products-card/products-card.component.ts
+++ src/app/components/products-card/products-card.component.ts
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductsCardComponent {
   @Input() imgCard: number = 1;
+  @Input() text: string = '';
 }
--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -9,4 +9,10 @@ export class ContentService {
   banner(): Observable<any> {
     return this.HttpClient.get('https://api.npoint.io/fee177346e7875554413');
   }
+  allNews(): Observable<any> {
+    return this.HttpClient.get('https://api.npoint.io/d275425a434e02acf2f7');
+  }
+  allCategories(): Observable<any> {
+    return this.HttpClient.get('https://api.npoint.io/91298d970c27e9a06518');
+  }
 }
